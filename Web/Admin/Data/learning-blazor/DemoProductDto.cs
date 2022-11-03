5dcf2663a16097a9a20ac613a570612f45460963
Houssnou <hosni.moubarak@gmail.com>
2022-11-03 00:10:19 -0700

    added two way binding sample code

---
+++ Web/Admin/Data/learning-blazor/DemoProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Admin.Data.learning_blazor
+{
+    public class DemoProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+        public double Price { get; set; }
+    }
+}
