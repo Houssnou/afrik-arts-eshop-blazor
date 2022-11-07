90ea92e2b5924a6bbf2ff210fcabda3e80233bdb
Houssnou <hosni.moubarak@gmail.com>
2022-11-06 22:52:19 -0800

    learning blazor -  data and prop binding

--- Web/Admin/Data/learning-blazor/DemoProductDto.cs
+++ Web/Admin/Data/learning-blazor/DemoProductDto.cs
@@ -6,5 +6,13 @@
         public string Name { get; set; }
         public bool IsAvailable { get; set; }
         public double Price { get; set; }
+        public List<DemoProductDetails> Details { get; set; }
+    }
+
+    public class DemoProductDetails
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
